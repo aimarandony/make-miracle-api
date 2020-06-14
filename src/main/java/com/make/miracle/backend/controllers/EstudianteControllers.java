7ee2a115b339a4b401dca19a5f28fc58a267285c
Aimar Andony <aimarandony@gmail.com>
2020-06-13 19:36:36 -0500

    add crossorigin netlify

--- src/main/java/com/make/miracle/backend/controllers/EstudianteControllers.java
+++ src/main/java/com/make/miracle/backend/controllers/EstudianteControllers.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.zip.DataFormatException;
 
-@CrossOrigin(origins = {"http://localhost:4200"})
+@CrossOrigin(origins = {"http://localhost:4200","https://makemiracle.netlify.app"})
 @RestController
 @RequestMapping("/app")
 public class EstudianteControllers {
